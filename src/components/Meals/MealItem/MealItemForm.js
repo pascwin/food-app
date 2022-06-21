c707121f7c9807c49657f8dfee683722bf964969
“Pascwin” <pascal-winner@web.de>
2022-06-21 13:51:31 +0200

    added Cart Modul to app

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -5,15 +5,18 @@ import classes from "./MealItemForm.module.css"
 const MealItemForm = (props) => {
     return (
         <form className={classes.form}>
-        <Input label="Amount" input={{
-            id: "amount",
-            type: "number",
-            min: "1",
-            max: "5",
-            step: "1",
-            defaultValue: "1"
-        }}/>
-        <button>+ Add</button>
+            <Input
+                label="Amount"
+                input={{
+                    id: "amount" + props.id,
+                    type: "number",
+                    min: "1",
+                    max: "5",
+                    step: "1",
+                    defaultValue: "1"
+                }}
+            />
+            <button>+ Add</button>
 
         </form>
     )
