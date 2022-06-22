ae22192dd72804b7d9c16331aeb16926e9672c6a
“Pascwin” <pascal-winner@web.de>
2022-06-22 20:29:26 +0200

    added state management useContext to add and remove items from Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,26 +1,51 @@
-import Modal from "../UI/Modal"
+import { useContext } from "react"
 
+import CartItem from "./CartItem"
+import Modal from "../UI/Modal"
 import classes from "./Cart.module.css"
+import CartContext from "../../store/cart-context"
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext)
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
     const orderFood = () => {
         console.log("ordering...")
     }
-    const cartItems = <ul className={classes["cart-items"]}>{[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map(item => {
-        return (
-            <li key={item.id}>{item.name}</li>
-        )
-    })}</ul>
+
+    const cartItemRemoveHandler = (id) => {
+        cartCtx.removeItem(id)
+    }
+
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem({...item, amount: 1})
+    }
+
+    const cartItems = <ul className={classes["cart-items"]}>
+        {cartCtx.items.map(item => {
+            return (
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
+            )
+        })}</ul>
     return (
         <Modal onClose={props.onClose}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes["button-alt"]} onClick={props.onClose}>Close</button>
-                <button className={classes.button} onClick={orderFood}>Order</button>
+                {hasItems && <button className={classes.button} onClick={orderFood}>Order</button>}
             </div>
         </Modal>
     )
